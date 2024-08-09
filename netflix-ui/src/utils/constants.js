d67bc34a0276cc35991c3a1c5c2195bb779a39d0
Raj Baghel <102679311+Raj-baghel123@users.noreply.github.com>
2024-08-09 10:36:21 +0530

    Delete netflix-ui directory

--- netflix-ui/src/utils/constants.js
+++
@@ -1,5 +0,0 @@
-export const API_KEY = "3d39d6bfe362592e6aa293f01fbcf9b9";
-export const TMDB_BASE_URL = "https://api.themoviedb.org/3";
-
-
-//My api -> export const API_KEY = "1e3a37f1aa6137db9b97eb49415ea311";
